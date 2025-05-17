e036c82e061dad2319abaf6317e0da1acad6127a
jvm2000 <minozajohnvincent2000@gmail.com>
2025-05-17 13:47:34 +0800

    fixed: header image

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,37 @@
+<style scoped>
+/* For Chrome, Edge, Safari */
+::-webkit-scrollbar {
+  width: 10px;
+  height: 10px; /* for horizontal scrollbar */
+}
+
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #888;
+  border-radius: 10px;
+  border: 2px solid #f1f1f1;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+</style>
+
 <template>
   <div class="py-24 w-full flex flex-col items-center">
     <div class="w-full max-w-5xl space-y-10 flex flex-col items-center">
-      <!-- This is the text intro  -->
       <div class="flex items-center">
         <img src="/hand.svg" alt="" class="mr-2">
 
         <p class="text-2xl">, My Name is Juan and I am a</p>
       </div>
 
-      <!-- This is the image center  -->
       <img src="/home-design.svg" alt="">
       
-      <!-- This is the footer  -->
-      <div class="w-full flex items-center justify-between">
+      <div class="w-full flex items-center justify-between px-14">
         <span class="text-2xl">Based in Cebu, PH</span>
 
         <div class="flex items-center space-x-4">
