cc5320a7a486823e2520cf3641076202a25f6968
jvm2000 <minozajohnvincent2000@gmail.com>
2025-05-17 19:09:00 +0800

    added: project contents

--- pages/projects.vue
+++ pages/projects.vue
@@ -2,28 +2,35 @@
 type Projects = {
   im1: string,
   im2: string,
-  im3: string,
   label: string,
   description: string,
   to: string
 }
 const projectsData = ref<Projects[]>([
   {
-    im1: '',
-    im2: '',
-    im3: '',
-    label: 'Milk Tea',
-    description: 'yo',
-    to: ''
+    im1: '/images/mt1.png',
+    im2: '/images/mt2.png',
+    label: 'Magic Milk Tea',
+    description: "Step into a world of enchantment where every sip tells a story and every moment is infused with magic. At Magic Milktea, we're more than just a cafe—we're a destination for those seeking a taste of whimsy and wonder.",
+    to: 'https://magic-milktea.vercel.app/'
   }, {
-    im1: '',
-    im2: '',
-    im3: '',
-    label: 'Milk Tea',
-    description: 'yo',
-    to: ''
+    im1: '/images/t1.png',
+    im2: '/images/t2.png',
+    label: 'EM Cosmetics',
+    description: "Discover the glow within. Elevate your skincare routine with our nourishing essentials, designed to reveal your skin's natural brilliance",
+    to: 'https://emcosmetics.vercel.app/'
+  }, {
+    im1: '/images/a1.png',
+    im2: '/images/a2.png',
+    label: 'AXIS Martial Arts',
+    description: "AXIS Martial Arts is a world-class FREE software platform that enables you to manage your school, your students and your membership fees.",
+    to: 'https://axismartialarts.com/'
   }
 ])
+
+function openTab(link: string) {
+  window.open(link)
+}
 </script>
 
 <template>
@@ -42,24 +49,23 @@ const projectsData = ref<Projects[]>([
           <div 
             class="w-[432px] h-[296px] border absolute border-black rounded-sm bg-white z-[1]" 
             :class="[index % 2 !== 0 ? 'left-0 top-0' : 'right-0 top-0']"
-          />
+          >
+            <img :src="proj.im1"  class="h-full w-auto object-cover">
+          </div>
 
           <div 
             class="w-[311px] h-[214px] border absolute border-black rounded-sm bg-white z-[2]"
             :class="[index % 2 !== 0 ? 'right-0 bottom-6' : 'left-0 bottom-6']"
-          />
-
-          <div 
-            class="w-[149px] h-[144px] border absolute border-black rounded-sm bg-white z-[3]"
-            :class="[index % 2 !== 0 ? 'left-4 bottom-0' : 'right-4 bottom-0']"
-          />
+          >
+            <img :src="proj.im2"  class="h-full w-auto object-cover">
+          </div>
         </div>
 
         <div class="space-y-8 flex flex-col w-[456px]">
           <p 
             class="text-6xl text-black font-bold w-full"
             :class="[index % 2 !== 0 ? 'text-right' : 'text-left']"
-          >Sample</p>
+          >{{ proj.label }}</p>
 
           <div 
             class="w-full flex"
@@ -72,12 +78,13 @@ const projectsData = ref<Projects[]>([
             class="text-xl font-light"
             :class="[index % 2 !== 0 ? 'text-right' : 'text-left']"
           >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            {{ proj.description }}
           </p>
 
           <div 
             class="w-full flex"
             :class="[index % 2 !== 0 ? 'justify-end' : 'justify-start']"
+            @click="openTab(proj.to)"
           >
             <button class="border-2 border-black w-56 py-2.5 justify-center ">
               <p class="text-xl">View Website</p>
