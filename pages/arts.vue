3bfbb946c60c27cadc57615339e5fe87ad93a00d
jvm2000 <minozajohnvincent2000@gmail.com>
2025-05-18 19:51:14 +0800

    added: created arts

--- pages/arts.vue
+++ pages/arts.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
-
+const artsData = ref([
+  '/arts/1.png',
+  '/arts/2.png',
+  '/arts/3.png',
+  '/arts/4.png',
+  '/arts/5.png',
+  '/arts/6.png',
+  '/arts/7.png',
+])
 </script>
 
 <template>
-  
+  <div class="pt-24 pb-48 w-full flex flex-col items-center">
+    <div class="max-w-5xl w-full grid grid-cols-3 gap-10">
+      <div v-for="art in artsData" class="w-[303px] h-[303px] border border-black">
+        <img :src="art" class="h-auto w-full object-cover" />
+      </div>
+    </div>
+  </div>
 </template>
